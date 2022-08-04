55ca8630c85a5e1a9c98cfd495221abfd457e3a0
Jordan <jordan@redbrickroadstudio.com>
2022-08-04 10:50:13 -0400

    checking in demo

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { DialogComponent } from "@syncfusion/ej2-vue-popups";
+import { ref } from 'vue'
+
+const dialog = ref<InstanceType<typeof DialogComponent>>();
+
+const isOpen = ref<boolean>(false);
+</script>
+
+<template>
+  <div>
+    <button @click="() => isOpen = !isOpen">Open dialog</button>
+    <DialogComponent header="Title" :ref="dialog" :visible="isOpen" :close-on-escape="true">
+      <p>some content</p>
+    </DialogComponent>
+
+  </div>
+</template>
---
+++ src/components/__tests__/TheWelcome.spec.js
@@ -0,0 +1,10 @@
+import { describe, it } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import TheWelcome from '../TheWelcome.vue'
+
+describe('TheWelcome', () => {
+  it('renders properly', () => {
+    mount(TheWelcome)
+  })
+})
